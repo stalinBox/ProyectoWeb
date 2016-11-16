56aa343c9ece05169cc7c6200d05cf150dcea114
stalinBox <stalin_ramirez@yahoo.es>
2016-11-16 17:43:11 -0500

    parametros
    guardar todos los parametros

---
+++ src/main/java/com/project/mb/OrdenProcesoBean.java
@@ -0,0 +1,17 @@
+package com.project.mb;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+@ManagedBean
+@ViewScoped
+public class OrdenProcesoBean implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	// VARIABLES
+	// CONSTRUCTORES
+	// METODOS
+	// SETTERS AND GETTERS
+}
---
+++ src/main/java/com/project/mb/ParamTurnosBean.java
@@ -0,0 +1,42 @@
+package com.project.mb;
+
+import java.io.Serializable;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+
+import org.primefaces.event.CellEditEvent;
+import org.primefaces.event.RowEditEvent;
+
+@ManagedBean
+@ViewScoped
+public class ParamTurnosBean implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	// VARIABLES
+	// CONSTRUCTORES
+	// METODOS
+	public void onRowEdit(RowEditEvent event) {
+		FacesMessage msg = new FacesMessage("Editado", "Row Edit");
+		FacesContext.getCurrentInstance().addMessage(null, msg);
+	}
+
+	public void onRowCancel(RowEditEvent event) {
+		FacesMessage msg = new FacesMessage("Edit Cancelado", "RowCancel");
+		FacesContext.getCurrentInstance().addMessage(null, msg);
+	}
+
+	public void onCellEdit(CellEditEvent event) {
+		Object oldValue = event.getOldValue();
+		Object newValue = event.getNewValue();
+
+		if (newValue != null && !newValue.equals(oldValue)) {
+			FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO,
+					"Cell Changed", "Old: " + oldValue + ", New:" + newValue);
+			FacesContext.getCurrentInstance().addMessage(null, msg);
+		}
+	}
+	// SETTERS AND GETTERS
+}
